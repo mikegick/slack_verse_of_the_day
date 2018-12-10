285d154af7b7e90d5fae5d621b28015a07a63810
Michael Gicking <mgicking@catalyte.io>
2018-12-10 16:35:45 -0500

    Adding multi-channel support and fixing issue with json config

--- runner.py
+++ runner.py
@@ -4,29 +4,38 @@ import sys
 import operator
 import random
 import string
+import os
+import json
+import datetime
 from slackclient import SlackClient
 
 verseURL = "https://beta.ourmanna.com/api/v1/get/?format=json"
-settings = open("settings.json", "r").json()
+with open(os.path.join(os.path.dirname(os.path.realpath('__file__')) + "\config\settings.json")) as settings:
+    jsonSettings = json.load(settings)
 #--------------------------------------------------------------------------------------------------------------------------|
 #DeprecationWarning: Slack Tokens are aimed to be replaced in version 2.0 of this app which will aim to use 0Auth2 instead.|
 #0Auth2 information will be stored in credentials.json in the future                                                       |
-slackToken = settings["slackToken"] #                                                                                      |
+    slackToken = jsonSettings["slackToken"] #                                                                       |
 #--------------------------------------------------------------------------------------------------------------------------|
+    channelsToPostTo = jsonSettings["channelList"]
 sc = SlackClient(slackToken)
-channelToPostTo = "general"
+
 
 while(True):
-    
-    r = requests.get(url = verseURL) 
-    data = r.json()
-    verse = "\"" + data["verse"]["details"]["text"] + "\" -" + data["verse"]["details"]["reference"]
-    
-    
-    sc.api_call(
-      "chat.postMessage",
-      channel=channelToPostTo,
-      text=verse,
-      username="Verse of the Day"
-    )
-    time.sleep(86400)
+    dayAndTime = datetime.datetime.now()
+    print (dayAndTime.hour)
+    if(dayAndTime.hour == 12):
+        r = requests.get(url = verseURL) 
+        data = r.json()
+        verse = "\"" + data["verse"]["details"]["text"] + "\" -" + data["verse"]["details"]["reference"]
+        
+        for channel in channelsToPostTo:
+            sc.api_call(
+              "chat.postMessage",
+              channel=channel,
+              text=verse,
+              username="Verse of the Day"
+            )
+        time.sleep(86300)
+    else:
+        time.sleep(99)
